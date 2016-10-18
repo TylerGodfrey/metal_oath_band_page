80f92e946a8ea80b5cab3c0fbec4f127c35e49cd
SC\justin.hovey <justin.hovey@my.simpson.edu>
2016-10-18 11:27:41 -0500

    Created deploy_script

---
+++ public_html/deploy_script.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Deployment Script</title>
+	</head>
+	<body>
+	<p>Deploy script</p>
+		<pre>
+		
+<?php
+$output = shell_exec("cd ..;git pull 2>&1");
+echo $output;
+?>
+		
+		</pre>
+	</body>
+</html><!DOCTYPE html>
+
+<html lang="en">
+	<head>
+		<title>Deployment Script</title>
+	</head>
+	<body>
+	<p>Deploy script</p>
+		<pre>
+		
+<?php
+$output = shell_exec("cd ..;git pull 2>&1");
+echo $output;
+?>
+		
+		</pre>
+	</body>
+</html>
